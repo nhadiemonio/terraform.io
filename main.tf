49da69d40d26ddb26a3bcd05d5efceae844eac41
ronaldo.ramos <ron.ramos@myrepublic.net>
2020-04-27 22:58:07 +0800

    provider tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "nhadiemonio"
+    workspaces {
+      name = "test"
+    }
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  version = "~> 2.8"
+  region = "ap-southeast-2"
+  profile = "default"
+}
