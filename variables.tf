56940fbf1b13b4a38561a26763045f09b9cb3718
ronaldo.ramos <ron.ramos@myrepublic.net>
2020-04-27 23:27:00 +0800

    set default variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "key-pairs-vars" {
   type = map
+  default = {
+    user1 = {
+      key_name = "user1"
+      pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN7fwbzIOEiEHxMbw0CWmFjnzmjbVdNLZ8m2srO/NE0bTb9lWd8L4QtcWAAmuToatR0ZQE5Tuhk0diuUMaDsJLOpuED7Z+6dPbIVd8ge+OcCU1KQUDjJC5I3grNEedROfPyYvlkjh1JU0ZgSB8prqActR57J59zAhzSfIuM5zOZL1NUNLGR2CLbLFZR5cB1atL2OLOuAEKl/u4yTYgr4hwcSLNbw5gvN1VZSqnAbPyT0B6UgjAp/Irlg2OFnV39ZHzbiri2ZJ7GOGs9wyQ4MNGspkm7B0bX1YFpuFy5N8SBkjWitcJum/9cgO2C5yuAXtWfWAwGK4+krG+I1B3ximt rramos"
+    }
+  }
 }
